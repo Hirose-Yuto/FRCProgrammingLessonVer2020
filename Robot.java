6870497f50a446af519171394ffeeea1f47810cf
Hirose-Yuto <hirose.y.aj@m.titech.ac.jp>
2020-10-09 22:34:49 +0900

    drive

--- Robot.java
+++ Robot.java
@@ -1,11 +1,32 @@
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class Robot extends TimedRobot {
-  
+  XboxController controller;
+  WPI_TalonSRX driveLeftFrontMotor, driveRightFrontMotor;
+  VictorSPX driveLeftBackMotor, driveRightBackMotor;
+  DifferentialDrive drive;
+
   @Override
   public void robotInit() {
+    controller = new XboxController(0);
+
+    driveLeftFrontMotor = new WPI_TalonSRX(6);
+    driveRightFrontMotor = new WPI_TalonSRX(2);
+    driveLeftBackMotor = new VictorSPX(13);
+    driveRightBackMotor = new VictorSPX(12);
+
+    driveLeftBackMotor.follow(driveLeftFrontMotor);
+    driveRightBackMotor.follow(driveRightFrontMotor);
+
+    drive = new DifferentialDrive(driveLeftFrontMotor, driveRightFrontMotor);
   }
 
 	@Override
@@ -22,6 +43,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
+    drive.arcadeDrive(-controller.getY(Hand.kLeft), controller.getX(Hand.kLeft));
   }
   
   @Override
